ac06c02a0c377efc9371f77590196fb22f853437
Erik Florida <erik.florida@qmerit.com>
2021-05-24 09:39:40 -0700

    added first api for connecting to cloudmailin

--- server/dirt.js
+++ server/dirt.js
@@ -1,14 +1,15 @@
 const express = require('express')
+const landflip = require('./landflip')
 
 let router = express.Router()
 
-router.post('/email', async (data) => {
+router.post('/landflip', async (data) => {
     try {
-        const emailData = await handleEmail(data)
-        const aggregateData = await getWebsiteData(emailData)
-        const result = await sendDataToZappier(aggregateData)
-        res.statusCode = result.ok ? 200 : 503
-        res.json({ result: 'success', data: aggregateData })
+        const emailData = await landflip.handleEmail(data)
+        const aggregateData = await landflip.getWebsiteData(emailData)
+        const zapierResponse = await landflip.sendDataToZappier(aggregateData)
+        res.statusCode = zapierResponse ? 200 : 503
+        res.json({ result: 'success', data: aggregateData, zapierResponse })
     } catch (error) {
         res.statusCode = 503
         res.send({ error: 'something went wrong...', response: error })
---
+++ server/landflip.js
@@ -0,0 +1,55 @@
+export const handleEmail = async (data) => {
+    console.log('data :: ', data)
+    return data
+}
+
+export const getWebsiteData = async (data) => {
+    const e = require('express')
+    const puppeteer = require('puppeteer-extra')
+    const stealth = require('puppeteer-extra-plugin-stealth')()
+
+    stealth.onBrowser = () => {}
+    puppeteer.use(stealth)
+
+    const setupPage = async (browser) => {
+        const page = await browser.newPage()
+        await page.setViewport({ width: 1200, height: 800 })
+        await page.setRequestInterception(true)
+        page.on('request', (req) => {
+            req.resourceType() == 'stylesheet' || req.resourceType() == 'image' || req.resourceType() == 'font' ? req.abort() : req.continue()
+        })
+
+        return page
+    }
+
+    const getBrowser = () =>
+        process.env.NODE_ENV === 'production'
+            ? puppeteer.connect({ browserWSEndpoint: `wss://chrome.browserless.io?token=${process.env.BROWSERLESSIO_KEY}` })
+            : puppeteer.launch({ headless: true })
+
+    return getBrowser().then(async (browser) => {
+        console.log('pp :: browser launched')
+        let page = await setupPage(browser)
+        console.log('pp :: page setup')
+        await page.goto(data.url)
+
+        console.log('pp :: search result loaded')
+        await page.waitForSelector('#foobar')
+        console.log('pp :: home loaded')
+        await page.evaluate(() => document.querySelector('#foobar').click())
+
+        await page.type('#username', supplierData.solar.userName)
+        await page.type('#password', supplierData.solar.password)
+        console.log('pp :: form filled out')
+
+        return availableQuantity
+    })
+}
+
+export const sendDataToZappier = async () => {}
+
+export default {
+    handleEmail,
+    getWebsiteData,
+    sendDataToZappier,
+}
